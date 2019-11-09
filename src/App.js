2c2aa86358424def40c966e710542214a063fad9
ArmenianDavid <imast.un@mail.ru>
2019-11-09 22:38:54 +0400

    'last_part'

--- src/App.js
+++ src/App.js
@@ -12,37 +12,52 @@ const App  = () =>{
   
   const [recipes,setRecipes] = useState([]);
   const [search,setSearch] = useState('');
+  const [query,setQuery] = useState('');
 
 
   useEffect(() => {
     getRecipes();
    
-  },[])
+  }, [query] )
+ 
 
   const getRecipes = async () =>{
-     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
      const data = await response.json();
    
      setRecipes(data.hits);
         console.log(data.hits)
   }
+
+  const updateSearch = (event) =>{
+       setSearch(event.target.value);
+       console.log(search)
+  }
+
+  const getSearch = (event) =>{
+      event.preventDefault();
+      setQuery(search);
+      setSearch(' ');
+  }
   
 
   return(
-    <div className='App' >
-      <form className='search-form'>
-        <input className='search-bar' type='text' />
+    <div className='app' >
+      <form onSubmit={getSearch} className='search-form'>
+        <input className='search-bar' type='text' value={search} onChange={updateSearch}  />
         <button className='search-button' type='submit'>Find</button>
       </form>
-      
+      <div className='recipes'>
       {recipes.map( recipe =>(
           <Recipe
              key={recipe.recipe.label} 
              title={recipe.recipe.label} 
              calories={recipe.recipe.calories}
              image={recipe.recipe.image}
+             ingredients={recipe.recipe.ingredients}            
              />
       ))}
+      </div>
     </div>
   )
 }
--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import '../recipe.modul.css';
 
-const Recipe = (props) =>{
+const Recipe = ({title,calories,image,ingredients}) =>{
+   
+  
+
+
   return(
-    <div >
-      <h1>{props.title}</h1>
-      <p>{props.calories}</p>
-      <img src={props.image} alt='img' />
+    <div className='recipe'>
+      <h1>{title}</h1>
+      <ol>Ingredients
+        {ingredients.map( ingredient =>{
+          
+        return <li key={ingredient.text}> {ingredient.text} <span className='weight'>Weight</span>: {Math.floor(ingredient.weight)} gram </li>
+        })}
+      </ol>
+      <p>{calories}</p>
+      <img className='image' src={image} alt='img' />
     </div>
   )
 }
