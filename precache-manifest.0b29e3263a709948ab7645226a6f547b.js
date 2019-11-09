5fe4fd2cdc09af2dfbd77647a40b76da0919ddf6
ArmenianDavid <imast.un@mail.ru>
2019-11-09 22:47:14 +0400

    Updates

---
+++ precache-manifest.0b29e3263a709948ab7645226a6f547b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2e23b898fff1bd93883accf3bea144",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "bdf0477c63731f41741f",
+    "url": "/recipes/static/css/main.58766cdc.chunk.css"
+  },
+  {
+    "revision": "efdd687ddc114a9ae77e",
+    "url": "/recipes/static/js/2.41bbc672.chunk.js"
+  },
+  {
+    "revision": "bdf0477c63731f41741f",
+    "url": "/recipes/static/js/main.f09cffda.chunk.js"
+  },
+  {
+    "revision": "f4eaa963a1143725175f",
+    "url": "/recipes/static/js/runtime-main.4e3caca3.js"
+  }
+]);
