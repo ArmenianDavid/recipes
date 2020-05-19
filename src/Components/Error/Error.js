c6639c7f6188b693fb54bd25234aad9d83848d9f
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-19 17:54:03 +0400

    Add Error component

---
+++ src/Components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <div className="error-container">
+            <h2>Error 404 server problems</h2>
+        </div>
+    );
+};
+
+export default Error;
