6557a24146c22c3a97945591474a9aa12e47376c
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-20 14:34:49 +0400

    Add unique key for recipe

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,8 +1,12 @@
 import React from "react";
+// components
+import Error from "../Error/Error";
 import Loading from "../Loading/Loading";
+import NoData from "../NoData/NoData";
 import Recipe from "../Recipe";
+import uuid from "react-uuid";
 
-const List = ({ loading, recipes }) => {
+const List = ({ loading, recipes, err }) => {
     if (loading) {
         return (
             <div>
@@ -10,11 +14,17 @@ const List = ({ loading, recipes }) => {
             </div>
         );
     }
+    if (err) {
+        return <Error err={err} />;
+    }
+    if (recipes.length === 0) {
+        return <NoData />;
+    }
     return (
         <div className="recipes">
             {recipes.map((recipe) => (
                 <Recipe
-                    key={recipe.recipe.label}
+                    key={uuid()}
                     title={recipe.recipe.label}
                     calories={recipe.recipe.calories}
                     image={recipe.recipe.image}
