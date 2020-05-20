a153a52d59bd31c0cdfa4981c7f09fac3ab9cdde
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-05-20 14:35:39 +0400

    Refactor Error component

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./Error.css";
 
-const Error = () => {
+const Error = ({ err }) => {
     return (
         <div className="error-container">
             <h2>Error 404 server problems</h2>
